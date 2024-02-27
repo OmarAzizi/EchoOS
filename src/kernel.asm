e3472d567c338c25782bc83fa563b223dc88c945
OmarAzizi <oalazizi75@gmail.com>
2024-02-27 22:32:37 +0300

    Kernel code

---
+++ src/kernel.asm
@@ -0,0 +1,24 @@
+BITS 32
+
+section     .data
+    CODE_SEG    EQU     0x08
+    DATA_SEG    EQU     0x10
+
+section     .text
+global _start
+_start:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov ebp, 0x00200000
+    mov esp, ebp
+
+    ; Enable the A20 line
+    in al, 0x92
+    or al, 2
+    out 0x92, al
+
+    jmp $
