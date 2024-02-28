3a20aa259c058831cd3ac5c76e9eb3da745f284d
OmarAzizi <oalazizi75@gmail.com>
2024-02-28 23:47:48 +0300

    Using C in the kernel

--- src/kernel.asm
+++ src/kernel.asm
@@ -1,5 +1,7 @@
 BITS 32
 
+extern  kernel_main
+
 section     .data
     CODE_SEG    EQU     0x08
     DATA_SEG    EQU     0x10
@@ -7,20 +9,21 @@ section     .data
 section     .asm
 global _start
 _start:
-    mov ax, DATA_SEG
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ss, ax
-    mov ebp, 0x00200000
-    mov esp, ebp
+    mov     ax, DATA_SEG
+    mov     ds, ax
+    mov     es, ax
+    mov     fs, ax
+    mov     gs, ax
+    mov     ss, ax
+    mov     ebp, 0x00200000
+    mov     esp, ebp
 
     ; Enable the A20 line
-    in al, 0x92
-    or al, 2
-    out 0x92, al
-
-    jmp $
+    in      al, 0x92
+    or      al, 2
+    out     0x92, al
+    
+    call    kernel_main
+    jmp     $
 
 times 512-($ - $$) db 0
---
+++ src/kernel.h
@@ -0,0 +1,11 @@
+#ifndef __KERNEL_H__
+#define __KERNEL_H__
+
+#include <stdint.h>
+
+#define VGA_WIDTH  80
+#define VGA_HEIGHT 20
+
+void kernel_main();
+
+#endif
