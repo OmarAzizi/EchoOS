8f80fddfc40ea35edbc91c2cc62414492dd453a9
OmarAzizi <oalazizi75@gmail.com>
2024-02-29 00:24:47 +0300

    Modified header, and added some new functions to help printing to the terminal

--- src/kernel.c
+++ src/kernel.c
@@ -1,23 +1,61 @@
 #include "kernel.h"
 #include <stdint.h>
+#include <stddef.h>
 
 uint16_t* video_mem = 0; 
+uint16_t terminal_row = 0;
+uint16_t terminal_col = 0;
+
+void next_row() {
+    terminal_row += 1;
+    terminal_col = 0;
+}
 
 // Will return the value of char:color to print in little-endian
 uint16_t terminal_make_char(char c, char color) {  return (color << 8) | c; }
+
+void terminal_putchar(int x, int y, char c, char color) {
+    video_mem[(y * VGA_WIDTH) + x] = terminal_make_char(c, color);
+}
+
+void terminal_writechar(char c, char color) {
+    if (c == '\n') {
+        next_row();
+        return;
+    }
+
+    terminal_putchar(terminal_col, terminal_row, c, color);
+    terminal_col += 1;
+    if (terminal_col >= VGA_WIDTH) next_row();
+}
 
 /*
     This will loop over the teminal and clear it all for us
 */
 void terminal_initialize() {
+    terminal_row = 0;
+    terminal_col = 0;
+
     video_mem = (uint16_t*)(0xB8000); // VGA 16-color text mode
     for (int y = 0; y < VGA_HEIGHT; ++y) {
-        for (int x = 0; x < VGA_WIDTH; ++x) 
-            video_mem[(y * VGA_WIDTH)  + x] = terminal_make_char(' ', 0);
+        for (int x = 0; x < VGA_WIDTH; ++x) terminal_putchar(x, y, ' ', 0);
     }
  
 }
+
+size_t strlen(const char* str) {
+    size_t len = 0;
+    while (str[len]) ++len;
+    return len;
+}
+
+void print(const char* str) {
+    size_t len = strlen(str);
+    for (int i = 0; i < len; ++i) 
+        terminal_writechar(str[i], 15); 
+}
+
 void kernel_main() {
     terminal_initialize();
-    video_mem[0] = terminal_make_char('B', 3);
+    print("Hello, World!\nMy Name is Omar :))");
 }
--- src/kernel.h
+++ src/kernel.h
@@ -1,8 +1,6 @@
 #ifndef __KERNEL_H__
 #define __KERNEL_H__
 
-#include <stdint.h>
-
 #define VGA_WIDTH  80
 #define VGA_HEIGHT 20
 
