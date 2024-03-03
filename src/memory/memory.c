50fd870685d5e7f7ddc845dc955519690067a01d
OmarAzizi <oalazizi75@gmail.com>
2024-03-03 23:06:46 +0300

    Updated

--- src/memory/memory.c
+++ src/memory/memory.c
@@ -3,7 +3,7 @@
 /*
     similar to memset in C stdlib
 */
-void* memset(void *ptr, int c, size_t size) {
+void* memset(void* ptr, int c, size_t size) {
     char* c_ptr = (char*)ptr;
     for (int i = 0; i < size; ++i) 
         c_ptr[i] = (char)c;
