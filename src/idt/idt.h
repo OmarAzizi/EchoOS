28b8185751bb523df1e708337145348f716a5137
OmarAzizi <oalazizi75@gmail.com>
2024-03-03 23:08:44 +0300

    added some macros

--- src/idt/idt.h
+++ src/idt/idt.h
@@ -1,6 +1,13 @@
 #ifndef __IDT_H__
 #define __IDT_H__
 
+#define     PIC1		    0x20		/* IO base address for master PIC */
+#define     PIC2		    0xA0		/* IO base address for slave PIC */
+#define     PIC1_COMMAND    PIC1
+#define     PIC1_DATA	    PIC1 + 1
+#define     PIC2_COMMAND	PIC2
+#define     PIC2_DATA	    PIC2 + 1
+
 #include <stdint.h>
 
 /*
@@ -27,5 +34,7 @@ struct idtr_desc {
 } __attribute__((packed));
 
 void idt_init();
+void enable_interrupts();
+void disable_interrupts();
 
 #endif
