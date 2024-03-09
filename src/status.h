03473beccedc2aff73590f4225132e4c95ecea2d
OmarAzizi <oalazizi75@gmail.com>
2024-03-09 22:32:13 +0300

    kernel status codes

---
+++ src/status.h
@@ -0,0 +1,10 @@
+#ifndef __STATUS_H__
+#define __STATUS_H__
+
+#define     ECHO_OS_ALL_OK      0
+#define     EIO                 1
+#define     EINVARG             2
+#define     ENOMEM              3
+
+
+#endif
