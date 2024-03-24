6dddb4f25ede4cd60ccd665604f6e54b8d7608e4
OmarAzizi <oalazizi75@gmail.com>
2024-03-24 15:45:05 +0300

    Added a new routine 'memset' similar to the one in C

--- src/memory/memory.c
+++ src/memory/memory.c
@@ -9,3 +9,12 @@ void* memset(void* ptr, int c, size_t size) {
         c_ptr[i] = (char)c;
     return ptr;
 }
+
+int memcmp(void* s1, void* s2, int count) {
+    char* c1 = s1;
+    char* c2 = s2;
+    while (count-- > 0) {
+        if (*c1++ != *c2++) return c1[-1] < c2[-1] ? -1 : 1;
+    }
+    return 0;
+}
